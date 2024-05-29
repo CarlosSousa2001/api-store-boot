a018e1081d01f604086302c97f701227786a492b
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-05-29 11:49:26 -0300

    mock de dados das entidades base

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO Category (name_category)
+VALUES ('Eletrônicos'), ('Roupas'), ('Livros');
+
+INSERT INTO payment (status)
+VALUES ('PENDING'), ('PROCESSING'), ('COMPLETED'), ('FAILED'), ('CANCELLED'), ('REFUNDED');
+
+INSERT INTO product (title, description, price, created_at)
+VALUES ('Titulo do Produto', 'Descrição do Produto', 100, '2024-05-29T00:00:00');
+
+INSERT INTO users (username, email, password, role)
+VALUES ('mockuser', 'mockuser@example.com', 'mockpassword', 'ROLE_CLIENTE');
+
+
