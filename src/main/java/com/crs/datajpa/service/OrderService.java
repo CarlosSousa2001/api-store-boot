e8cea5d1cf6de78802f82aefcfc136a3b0db664d
CarlosSousa2001 <carlosramos10k@gmail.com>
2024-07-01 10:22:52 -0300

    refactor: service order -> metodo verificador de cart

--- src/main/java/com/crs/datajpa/service/OrderService.java
+++ src/main/java/com/crs/datajpa/service/OrderService.java
@@ -25,35 +25,18 @@ public class OrderService {
     private CustomerService customerService;
 
     @Autowired
-    private CustomerRepository customerRepository;
-
-    @Autowired
-    private CartService cartService;
-
-    @Autowired
     private CartRepository cartRepository;
 
     @Autowired
     private ProductRepository productRepository;
 
-    @Autowired
-    private PaymenteRepository paymenteRepository;
 
     @Transactional
-    public OrderDTO createOrder(OrderDTO addOrder){
+    public OrderDTO createOrder(OrderDTO addOrder) {
 
         Customer customer = customerService.authenticated();
 
-        Cart cart = getCart(addOrder.getCartId());
-
-        // Se o usuário existir e o carrinho não tiver um usuário associado, associar o usuário ao carrinho
-        if (cart.getCustomer() == null) {
-            cart.setCustomer(customer);
-            cartRepository.save(cart); // Atualiza o carrinho com o usuário associado
-        }
-        
-        // preciso verificar se caso o cart ja tenha um usuario associado é o mesmo id do usuario que eu mandei no json
-        if(cart.getCustomer() != customer) throw new EntityNotFoundException();
+        Cart cart = getCart(addOrder.getCartId(), customer);
 
         Order order = new Order();
         order.setMoment(Instant.now());
@@ -61,38 +44,43 @@ public class OrderService {
 
         Payment payment = new Payment();
         payment.setStatus(PaymentStatus.PROCESSING);
-
         order.setPayment(payment);
 
-
-        for (CartItem item: cart.getCartItems()){
+        for (CartItem item : cart.getCartItems()) {
 
             Product product = productRepository.getReferenceById(item.getProduct().getId());
 
             OrderItem orderItem = new OrderItem();
 
             orderItem.setOrder(order);
-            orderItem.setProduct(item.getProduct());
-//          orderItem.setOrder(savedOrder); o jpa vai persistir para mim a ordem devido o uso do cascade.all
-
+            orderItem.setProduct(product);
             orderItem.setPrice(item.getPrice());
             orderItem.setQuantity(item.getQuantity());
             orderItem.setSize(item.getSize());
 
-
             order.getOrderItems().add(orderItem);
         }
-
 
         orderRepository.save(order);
         orderItemRepository.saveAll(order.getOrderItems());
 
-       return new OrderDTO(order);
+        return new OrderDTO(order);
 
     }
 
-    private Cart getCart(Long id){
-        return cartRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+    private Cart getCart(Long id, Customer customer) {
+        Cart cart = cartRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
+
+        // Se o usuário existir e o carrinho não tiver um usuário associado, associar o usuário ao carrinho
+        if (cart.getCustomer() == null) {
+            cart.setCustomer(customer);
+            cartRepository.save(cart); // Atualiza o carrinho com o usuário associado
+        }
+
+        // preciso verificar se caso o cart ja tenha um usuario associado é o mesmo id do usuario que eu mandei no json
+        if (!cart.getCustomer().equals(customer)) throw new EntityNotFoundException();
+
+        return cart;
     }
 
 }
